cb1ef1c9ec7de68d4893d4bca8980ea3c79fbc93
Jonathan Wong <jonathan.wong@applydigital.co>
2020-09-28 01:49:38 -0400

    Fix samesite cookies..rename forgot password folder

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -15,10 +15,11 @@ const createSendToken = (user, res, statusCode, req) => {
 
   const jwtOptions = {
     httpOnly: true,
+    sameSite: 'None',
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    // secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
   };
 
   res.cookie('jwt', token, jwtOptions);
